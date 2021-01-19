9ceafc39ac27f02b54723bac187c615576e257c5
m <m@metaquanta.com>
2021-01-18 19:36:25 -0500

    use gssdp if available

--- bin/launch_castcontrol.sh
+++ bin/launch_castcontrol.sh
@@ -3,8 +3,7 @@
 self=$( which $0 )
 path=$( realpath "${self%/*}"/.. )
 
-fn='[^"]*'
-port=8809
+port=8009
 
 while (( "$#" )); do
     case "$1" in
@@ -37,17 +36,71 @@ EOF
     esac
 done
 
-if [ -z $ip ]; then 
-    # TODO: ought to handle multiple lines of Chromecasts.
-    eval $( avahi-browse --resolve -ktp _googlecast._tcp | sed -Ene "s/^=.*;([^;]*);([^;]*);.*\"fn=($fn)\".*$/ip=\1\nport=\2\nfn=\"\3\"/p" )
+function _avahi {
+    local fn_search
+    local _t
+    if [ -z "$fn" ]; then
+        fn_search='[^"]*'
+    else
+        fn_search="$fn"
+    fi
+    IFS="\n"
+    for cast in "$( 
+            avahi-browse --resolve -ktp _googlecast._tcp | \
+            sed -Ene "s/^=.*;([^;]*);([^;]*);.*\"fn=($fn_search)\".*$/\1:\2:\3/p" \
+        )"
+    do
+        _t=${cast%:*}
+        ip=${_t%:*}
+        port=${_t#*:}
+        fn=${cast##*:}
+        break
+    done
+}
+
+function _gssdp {
+    local urls=$( 
+        gssdp-discover \
+          -m available \
+          -t urn:dial-multiscreen-org:device:dial \
+          -n 1 | \
+        sed -Ene 's/^\s*Location:\s(\S*).*$/\1/p' \
+    )
+    local fn_search
+    local _t
+    local _fn
+    if [ -n "$fn" ]; then
+        fn_search="$fn"
+    else
+        fn_search=".*"
+    fi
+    for url in "$urls"
+    do
+        _fn=$( 
+            curl -s $url | \
+            sed -Ene "s/^.*<friendlyName>($fn_search)<\/friendlyName>.*$/\1/p" \
+        )
+        if [ -n "$_fn" ]; then
+            _t=${url#*//}
+            ip=${_t%%:*}
+            fn="$_fn"
+            break
+        fi
+    done
+}
+
+if [ -z "$ip" ]; then 
+    if command -v avahi-browse > /dev/null; then
+        _avahi
+    elif command -v gssdp-discover > /dev/null; then
+        _gssdp
+    fi
 
     if [ -z $ip ]; then
         echo "No Chromecast found!"
         exit 1
     fi
-    echo "Found '$fn' at $ip:$port"
+    echo "Found '$fn' at $ip"
 fi
 
-${path}/build/castcontrol.js $ip $port
-
-
+"${path}/build/castcontrol.js" $ip $port
